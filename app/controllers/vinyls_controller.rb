032c73a64a0b148f3be9da0d2e158b6ae75fd3bd
Celine Brin <celine-brin@hotmail.fr>
2023-02-04 13:35:35 +0100

    ajout de vinyl ok

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,11 +1,13 @@
 class VinylsController < ApplicationController
   include Pagy::Backend
+  before_action :authenticate_user!
   before_action :set_user, only: [:index]
   before_action :set_vinyl, only: [:archives, :show, :edit, :update, :destroy]
   before_action :set_all_vinyls, only: [:index, :archives]
 
   def index
     @vinyls = @user.vinyls # associer les vinyls à l'user
+
     if params[:query].present?
       sql_query = "artist ILIKE :query OR album ILIKE :query"
       @vinyls = Vinyl.where(sql_query, query: "%#{params[:query]}%")
@@ -19,22 +21,34 @@ class VinylsController < ApplicationController
   end
 
   def new
+    @user = current_user
     @vinyl = Vinyl.new
   end
 
   def show
+    @vinyl = Vinyl.find(params[:id])
   end
 
   def create
-    @vinyl = Vinyl.new(vinyl_params)
-    # @vinyl.user = current_user
+    @vinyl = current_user.vinyls.build(vinyl_params)
     set_generic_photo unless @vinyl.photo.present?
-    if @vinyl.save!
-      redirect_to vinyls_path
+    if @vinyl.save
+      redirect_to @vinyl
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  # def create
+  #   @vinyl = Vinyl.new(vinyl_params)
+  #   @user = current_user
+  #   set_generic_photo unless @vinyl.photo.present?
+  #   if @vinyl.save
+  #     redirect_to vinyls_path(@vinyl)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
   def edit
   end
@@ -61,10 +75,6 @@ class VinylsController < ApplicationController
   def vinyl_params
     params.require(:vinyl).permit(:album, :artist, :commentaire, :photo)
   end
-
-  # def set_user
-  #   @user = current_user
-  # end
 
   def set_all_vinyls
     @vinyls = Vinyl.where(user_id: @user).order('date DESC')
